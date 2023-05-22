b602b9010a476bf32316e77fedaf797d043e2623
mscott999 <maddoxscott99@gmail.com>
2023-05-22 13:00:29 -0700

    Heuristic cutoff implemented.

--- Product/AI.java
+++ Product/AI.java
@@ -1,6 +1,10 @@
 public class AI {
-    static GameState returnMove(GameState state) {
-        return (GameState)minValueAB(state, Integer.MIN_VALUE, Integer.MAX_VALUE)[0];
+    static GameState returnMove(GameState state, int size) {
+        if (size > 3) {
+            return (GameState)minValueABCutoff(state, Integer.MIN_VALUE, Integer.MAX_VALUE, 13 - size)[0];
+        } else {
+            return (GameState)minValueAB(state, Integer.MIN_VALUE, Integer.MAX_VALUE)[0];
+        }
     }
 
     static Object[] maxValueAB(GameState state, int alpha, int beta) {
@@ -35,6 +39,50 @@ public class AI {
         }
         for (GameState successorState : state.getMoves('O')) {
             int successorEval = (Integer)maxValueAB(successorState, alpha, beta)[1];
+            if (successorEval < (Integer)output[1]) {
+                output[0] = successorState;
+                output[1] = successorEval;
+            }
+            beta = Integer.min(beta, successorEval);
+            if (alpha >= beta) {
+                break;
+            }
+        }
+        return output;
+    }
+
+    static Object[] maxValueABCutoff(GameState state, int alpha, int beta, int cutoff) {
+        Object[] output = new Object[2];
+        output[0] = null;
+        output[1] = Integer.MIN_VALUE;
+        if (state.isTerminalState() || cutoff == 0) {
+            output[1] = state.heuristicValue();
+            return output;
+        }
+        for (GameState successorState : state.getMoves('X')) {
+            int successorEval = (Integer)minValueABCutoff(successorState, alpha, beta, cutoff - 1)[1];
+            if (successorEval > (Integer)output[1]) {
+                output[0] = successorState;
+                output[1] = successorEval;
+            }
+            alpha = Integer.max(alpha, successorEval);
+            if (alpha >= beta) {
+                break;
+            }
+        }
+        return output;
+    }
+
+    static Object[] minValueABCutoff(GameState state, int alpha, int beta, int cutoff) {
+        Object[] output = new Object[2];
+        output[0] = null;
+        output[1] = Integer.MAX_VALUE;
+        if (state.isTerminalState() || cutoff == 0) {
+            output[1] = state.heuristicValue();
+            return output;
+        }
+        for (GameState successorState : state.getMoves('O')) {
+            int successorEval = (Integer)maxValueABCutoff(successorState, alpha, beta, cutoff - 1)[1];
             if (successorEval < (Integer)output[1]) {
                 output[0] = successorState;
                 output[1] = successorEval;
--- Product/Main.java
+++ Product/Main.java
@@ -10,17 +10,17 @@ public class Main {
         Scanner input = new Scanner(System.in);
         GameState state;
         System.out.println("Wlecome to Tic-Tac-Toe!\n__________________________________________________\nPlayer 1: X\nAI: O\n__________________________________________________");
+        int size;
         while (true) {
             System.out.print("How many tiles wide should the board be?: ");
             if (input.hasNextInt()) {
-                int size = input.nextInt();
+                size = input.nextInt();
                 input.nextLine();
-                if (size > 0 && size < 6) {
+                if (size > 0 && size <= 10) {
                     state = new GameState(size);
                     break;
                 } else {
-                    System.out.println("Invalid input: pleas select an integer between 1 and 5.");
-                    input.nextLine();
+                    System.out.println("Invalid input: please select an integer between 1 and 10.");
                 }
             } else {
                 System.out.println("Invalid input: please select an integer value.");
@@ -42,7 +42,7 @@ public class Main {
                 break; 
             }
             System.out.println("AI\'s Turn");
-            state = AI.returnMove(state);
+            state = AI.returnMove(state, size);
             state.printBoard();
             if (state.isTerminalState()) {
                 if (state.value() == 1) {
